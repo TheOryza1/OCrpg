ea093af1c09eac856a09683edb99988ea874ec6c
Kaneis Zontanos <ramim@bc.edu>
2023-11-26 17:33:00 -0500

    Initial Commit

---
+++ OCrpg/OCrpgApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCrpgApp.swift
+//  OCrpg
+//
+//  Created by Kaneis Zontanos on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct OCrpgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
