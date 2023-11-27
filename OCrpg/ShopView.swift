7d8de526131ffe51211ab239c7f0b5ec46218879
Kaneis Zontanos <ramim@bc.edu>
2023-11-26 19:59:24 -0500

    added shop view

---
+++ OCrpg/ShopView.swift
@@ -0,0 +1,18 @@
+//
+//  ShopView.swift
+//  OCrpg
+//
+//  Created by Kaneis Zontanos on 11/26/23.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopView()
+}
---
+++ OCrpg/ShopWidgets.swift
@@ -0,0 +1,18 @@
+//
+//  ShopWidgets.swift
+//  OCrpg
+//
+//  Created by Kaneis Zontanos on 11/26/23.
+//
+
+import SwiftUI
+
+struct ShopWidgets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopWidgets()
+}
